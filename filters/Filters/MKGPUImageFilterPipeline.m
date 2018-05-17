4e1800d54b51c3fea91b8952e49ee15df67d9b2a
ZhaoHang <18911728754@163.com>
2018-05-17 18:07:02 +0800

    l

--- filters/Filters/MKGPUImageFilterPipeline.m
+++ filters/Filters/MKGPUImageFilterPipeline.m
@@ -224,8 +224,7 @@
     } else {
         GPUImageToneCurveFilter *filter = [[GPUImageToneCurveFilter alloc] init];
         [filter setPointsWithACV:[NSString stringWithFormat:@"%@", name]];
-        //本地文件暂无，需要.ACV文件，老版本（年代久远(12年左右)）的.ramp文件新版本无法识别，文件中主要包含r，g，b等基本数据，新版本玩不转暂时
-        //return [[NSClassFromString([NSString stringWithFormat:@"%@", name]) alloc] init];
+        //本地文件暂不全，暂时研究ps，准备自己做，需要.ACV文件，老版本（年代久远(12年左右)）的.ramp文件新版本无法识别，文件中主要包含r，g，b等基本数据，新版本玩不转暂时
         return filter;
     }
     return nil;
